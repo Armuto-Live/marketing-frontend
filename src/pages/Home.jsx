e2364d0e75d888252551b1a7abf1414cac4221da
Armuto-Live <luisarmuto3@gmail.com>
2022-12-29 08:38:40 -0500

    add Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Announcement from '../components/Announcement/Announcement'
+import Navbar from '../components/Navbar/Navbar'
+export const Home = () => {
+    return (
+      <div>
+        <Announcement/>
+        <Navbar/>
+      </div>
+    )
+  }
